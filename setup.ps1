29f0cb2622336cdb588dd71f1e8fc285552b0261
thebananathief <cameron.salomone@gmail.com>
2023-03-06 20:40:27 -0500

    Added prints and fixed the windowsterminal config

--- setup.ps1
+++ setup.ps1
@@ -12,10 +12,11 @@ Copy-Item shell-setup\Microsoft.PowerShell_profile.ps1 $PROFILE -Force
 # Copy the WindowsTerminal config
 $WTFamilyName = $(Get-AppxPackage | Where-Object Name -eq Microsoft.WindowsTerminal).PackageFamilyName
 $WTData = "$env:LOCALAPPDATA\Packages\$WTFamilyName\LocalState"
-if (Test-Path "$WTData\settings.json") {
-    Write-Host "Found an existing WindowsTerminal config, renaming..."
-    Rename-Item "$WTData\settings.json" settings.json.bak
-}
+# if (Test-Path "$WTData\settings.json") {
+#     Write-Host "Found an existing WindowsTerminal config, renaming..."
+#     Rename-Item "$WTData\settings.json" settings.json.bak
+# }
+Write-Host "Copying WindowsTerminal config to $WTData..."
 Copy-Item shell-setup\WindowsTerminal\settings.json $WTData -Force
 # C:\Users\Cameron\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
 
@@ -23,7 +24,10 @@ Copy-Item shell-setup\WindowsTerminal\settings.json $WTData -Force
 # if (Test-Path ~/.config -eq $False) {
 #     New-Item ~/.config/
 # }
+Write-Host "Copying Starship config to $env:USERPROFILE\.config..."
 Copy-Item shell-setup\starship.toml ~\.config -Force
 
+Write-Host
+Write-Host "Finished! -- This repository now resides in $env:USERPROFILE\shell-setup"
 # Delete the setup repo
 # Remove-Item shell-setup -Recurse -Force
