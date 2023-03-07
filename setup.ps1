ea44cc3749310d76d15b92550c4df1362f9a0a78
thebananathief <cameron.salomone@gmail.com>
2023-03-06 20:54:48 -0500

    For Windows-Changed from downloading entire repo to downloading needed files

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 # Clone the repository
-git clone https://github.com/thebananathief/shell-setup
+# git clone https://github.com/thebananathief/shell-setup
 
 # Copy the powershell profile
 # if (Test-Path $PROFILE) {
@@ -7,7 +7,8 @@ git clone https://github.com/thebananathief/shell-setup
 #     Rename-Item $PROFILE Microsoft.PowerShell_profile.ps1.bak
 # }
 Write-Host "Copying PowerShell profile to $PROFILE..."
-Copy-Item shell-setup\Microsoft.PowerShell_profile.ps1 $PROFILE -Force
+# Copy-Item shell-setup\Microsoft.PowerShell_profile.ps1 $PROFILE -Force
+Invoke-WebRequest https://raw.githubusercontent.com/thebananathief/shell-setup/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
 
 # Copy the WindowsTerminal config
 $WTFamilyName = $(Get-AppxPackage | Where-Object Name -eq Microsoft.WindowsTerminal).PackageFamilyName
@@ -17,17 +18,19 @@ $WTData = "$env:LOCALAPPDATA\Packages\$WTFamilyName\LocalState"
 #     Rename-Item "$WTData\settings.json" settings.json.bak
 # }
 Write-Host "Copying WindowsTerminal config to $WTData..."
-Copy-Item shell-setup\WindowsTerminal\settings.json $WTData -Force
-# C:\Users\Cameron\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+# Copy-Item shell-setup\WindowsTerminal\settings.json $WTData -Force
+Invoke-WebRequest https://raw.githubusercontent.com/thebananathief/shell-setup/main/WindowsTerminal/settings.json -o "$WTData\settings.json"
 
 # Copy the starship config
 # if (Test-Path ~/.config -eq $False) {
 #     New-Item ~/.config/
 # }
 Write-Host "Copying Starship config to $env:USERPROFILE\.config..."
-Copy-Item shell-setup\starship.toml ~\.config -Force
+# Copy-Item shell-setup\starship.toml ~\.config -Force
+Invoke-WebRequest https://raw.githubusercontent.com/thebananathief/shell-setup/main/starship.toml -o "$env:USERPROFILE\.config\starship.toml"
 
 Write-Host
-Write-Host "Finished! -- This repository now resides in $env:USERPROFILE\shell-setup"
+Write-Host "Finished! -- Enjoy your pretty terminal!"
+# Write-Host "Finished! -- This repository now resides in $env:USERPROFILE\shell-setup"
 # Delete the setup repo
 # Remove-Item shell-setup -Recurse -Force
