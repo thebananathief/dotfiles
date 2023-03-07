bb16ab8161e32335772a24de0bdb27bf3b0a4b97
thebananathief <cameron.salomone@gmail.com>
2023-03-06 20:23:31 -0500

    Added windows terminal config

--- setup.ps1
+++ setup.ps1
@@ -1,23 +1,29 @@
-# Ensure home directory
-Set-Location ~
-
-# Clone the repository and navigate to it
+# Clone the repository
 git clone https://github.com/thebananathief/shell-setup
-Set-Location shell-setup
 
 # Copy the powershell profile
-if (Test-Path $PROFILE) {
-    Write-Host "Found an existing PowerShell profile, renaming..."
-    Rename-Item $PROFILE Microsoft.PowerShell_profile.ps1.bak
-}
+# if (Test-Path $PROFILE) {
+#     Write-Host "Found an existing PowerShell profile, renaming..."
+#     Rename-Item $PROFILE Microsoft.PowerShell_profile.ps1.bak
+# }
 Write-Host "Copying PowerShell profile to $PROFILE..."
-Copy-Item Microsoft.PowerShell_profile.ps1 $PROFILE
+Copy-Item shell-setup\Microsoft.PowerShell_profile.ps1 $PROFILE -Force
 
 # Copy the WindowsTerminal config
-
+$WTFamilyName = Get-AppxPackage | Where-Object Name -eq Microsoft.WindowsTerminal | Select-Object PackageFamilyName
+$WTData = "$env:LOCALAPPDATA\Packages\$WTFamilyName\LocalState"
+if (Test-Path "$WTData\settings.json") {
+    Write-Host "Found an existing WindowsTerminal config, renaming..."
+    Rename-Item "$WTData\settings.json" settings.json.bak
+}
+Copy-Item shell-setup\WindowsTerminal\settings.json $WTData -Force
+# C:\Users\Cameron\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
 
 # Copy the starship config
-
+# if (Test-Path ~/.config -eq $False) {
+#     New-Item ~/.config/
+# }
+Copy-Item shell-setup\starship.toml ~\.config -Force
 
 # Delete the setup repo
-Remove-Item . -Recurse -Force
+Remove-Item shell-setup -Recurse -Force
