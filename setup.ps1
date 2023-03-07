ab2bfeafa8ddc147f269e4dfdd39e20a4205e96b
thebananathief <cameron.salomone@gmail.com>
2023-03-06 19:53:11 -0500

    More powershell script additions

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,16 @@
+# Ensure home directory
 Set-Location ~
+
+# Clone the repository and navigate to it
 git clone https://github.com/thebananathief/shell-setup
 Set-Location shell-setup
-Copy-Item Microsoft.PowerShell_profile.ps1 ~/Documents
+
+# Copy the powershell profile
+if (Test-Path $PROFILE) {
+    Rename-Item $PROFILE Microsoft.PowerShell_profile.ps1.bak
+}
+Copy-Item Microsoft.PowerShell_profile.ps1 $PROFILE -verbose
+
+# Copy the WindowsTerminal config
+
+# Copy the starship config
