f9226fe3ffe5d1404a08cef8fc5e34d4a74a8be0
thebananathief <cameron.salomone@gmail.com>
2023-06-28 18:30:37 -0400

    Coc

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
   -- "folke/which-key.nvim",
   -- { "folke/neoconf.nvim", cmd = "Neoconf" },
   -- "folke/neodev.nvim",
-  -- "neoclide/coc.nvim",
+  "neoclide/coc.nvim",
   {'akinsho/toggleterm.nvim', version = "*",
     config = function()
       require("toggleterm").setup{
