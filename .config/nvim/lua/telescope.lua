67637ff79bfa0c9ae1d0bd7b7c9f2e99b3acc5e3
thebananathief <cameron.salomone@gmail.com>
2023-10-16 08:50:48 -0400

    add telescope config

---
+++ .config/nvim/lua/telescope.lua
@@ -0,0 +1,7 @@
+local map = vim.keymap.set
+local builtin = require('telescope.builtin')
+
+map('n', '<leader>ff', builtin.find_files, {})
+map('n', '<leader>fg', builtin.live_grep, {})
+map('n', '<leader>fb', builtin.buffers, {})
+map('n', '<leader>fh', builtin.help_tags, {})
