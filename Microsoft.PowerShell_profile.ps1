5a403cf92a6c703cf6a3885928c48f45ddc8d22f
thebananathief <cameron.salomone@gmail.com>
2024-02-01 20:11:43 -0500

    add starship and cleanup zoxide shit

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -157,8 +157,6 @@ function export($name, $value) {Set-Item -Force -Path "env:$name" -Value $value;
 function pkill($name) {Get-Process $name -ErrorAction SilentlyContinue | Stop-Process}
 function pgrep($name) {Get-Process $name}
 
-# Zoxide
-Invoke-Expression (& { (zoxide init --hook pwd powershell | Out-String) })
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
@@ -170,128 +168,9 @@ if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
 
-# =============================================================================
-#
-# Utility functions for zoxide.
-#
+# Starship
+Invoke-Expression (& 'C:\Program Files\starship\bin\starship.exe' init powershell --print-full-init | Out-String)
 
-# Call zoxide binary, returning the output as UTF-8.
-function global:__zoxide_bin {
-    $encoding = [Console]::OutputEncoding
-    try {
-        [Console]::OutputEncoding = [System.Text.Utf8Encoding]::new()
-        $result = zoxide @args
-        return $result
-    } finally {
-        [Console]::OutputEncoding = $encoding
-    }
-}
-
-# pwd based on zoxide's format.
-function global:__zoxide_pwd {
-    $cwd = Get-Location
-    if ($cwd.Provider.Name -eq "FileSystem") {
-        $cwd.ProviderPath
-    }
-}
-
-# cd + custom logic based on the value of _ZO_ECHO.
-function global:__zoxide_cd($dir, $literal) {
-    $dir = if ($literal) {
-        Set-Location -LiteralPath $dir -Passthru -ErrorAction Stop
-    } else {
-        if ($dir -eq '-' -and ($PSVersionTable.PSVersion -lt 6.1)) {
-            Write-Error "cd - is not supported below PowerShell 6.1. Please upgrade your version of PowerShell."
-        }
-        elseif ($dir -eq '+' -and ($PSVersionTable.PSVersion -lt 6.2)) {
-            Write-Error "cd + is not supported below PowerShell 6.2. Please upgrade your version of PowerShell."
-        }
-        else {
-            Set-Location -Path $dir -Passthru -ErrorAction Stop
-        }
-    }
-}
-
-# =============================================================================
-#
-# Hook configuration for zoxide.
-#
-
-# Hook to add new entries to the database.
-$global:__zoxide_oldpwd = __zoxide_pwd
-function global:__zoxide_hook {
-    $result = __zoxide_pwd
-    if ($result -ne $global:__zoxide_oldpwd) {
-        if ($null -ne $result) {
-            zoxide add -- $result
-        }
-        $global:__zoxide_oldpwd = $result
-    }
-}
-
-# Initialize hook.
-$global:__zoxide_hooked = (Get-Variable __zoxide_hooked -ErrorAction SilentlyContinue -ValueOnly)
-if ($global:__zoxide_hooked -ne 1) {
-    $global:__zoxide_hooked = 1
-    $global:__zoxide_prompt_old = $function:prompt
-
-    function global:prompt {
-        if ($null -ne $__zoxide_prompt_old) {
-            & $__zoxide_prompt_old
-        }
-        $null = __zoxide_hook
-    }
-}
-
-# =============================================================================
-#
-# When using zoxide with --no-cmd, alias these internal functions as desired.
-#
-
-# Jump to a directory using only keywords.
-function global:__zoxide_z {
-    if ($args.Length -eq 0) {
-        __zoxide_cd ~ $true
-    }
-    elseif ($args.Length -eq 1 -and ($args[0] -eq '-' -or $args[0] -eq '+')) {
-        __zoxide_cd $args[0] $false
-    }
-    elseif ($args.Length -eq 1 -and (Test-Path $args[0] -PathType Container)) {
-        __zoxide_cd $args[0] $true
-    }
-    else {
-        $result = __zoxide_pwd
-        if ($null -ne $result) {
-            $result = __zoxide_bin query --exclude $result -- @args
-        }
-        else {
-            $result = __zoxide_bin query -- @args
-        }
-        if ($LASTEXITCODE -eq 0) {
-            __zoxide_cd $result $true
-        }
-    }
-}
-
-# Jump to a directory using interactive search.
-function global:__zoxide_zi {
-    $result = __zoxide_bin query -i -- @args
-    if ($LASTEXITCODE -eq 0) {
-        __zoxide_cd $result $true
-    }
-}
-
-# =============================================================================
-#
-# Commands for zoxide. Disable these using --no-cmd.
-#
-
-Set-Alias -Name z -Value __zoxide_z -Option AllScope -Scope Global -Force
-Set-Alias -Name zi -Value __zoxide_zi -Option AllScope -Scope Global -Force
-
-# =============================================================================
-#
-# To initialize zoxide, add this to your configuration (find it by running
-# `echo $profile` in PowerShell):
-#
+# Zoxide
+Invoke-Expression (& { (zoxide init --hook pwd powershell | Out-String) })
 # Invoke-Expression (& { (zoxide init powershell | Out-String) })
